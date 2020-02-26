2ec76f9067e82dbf5dbe90312ce5f77ff627092e
Mariana Truta <marianatruta2000@gmail.com>
2020-02-26 12:03:05 +0000

    TP02

--- TP02/ex3.c
+++ TP02/ex3.c
@@ -7,27 +7,41 @@
 
 int main(int argc, char *argv[])
 {
- int fd1, nr, nw;
- unsigned char buffer[BUFFER_SIZE];
+    int fd1, fd2, nr, nw;
+    unsigned char buffer[BUFFER_SIZE];
 
- if (argc != 2) {
-  printf("Usage: %s <source>\n", argv[0]);
-  return 1;
- }
- fd1 = open(argv[1], O_RDONLY);
- if (fd1 == -1) {
-  perror(argv[1]);
-  return 2;
- }
-
- while ((nr = read(fd1, buffer, BUFFER_SIZE)) > 0){
-    if ((nw = write(STDOUT_FILENO, buffer, nr)) <= 0 || nw != nr) {
-        perror("Error");
-        close(fd1);
-        return 3;
+    if ((argc != 2) && (argc != 3)) {
+    printf("Usage: %s <source>  OR  %s <source> <destination>\n", argv[0], argv[0]);
+    return 1;
     }
-  }
-  close(fd1);
-  return 0;
+    fd1 = open(argv[1], O_RDONLY);
+    if (fd1 == -1) {
+        perror(argv[1]);
+        return 2;
+    }
+
+    if(argc == 3){
+
+        fd2 = open("outfile.txt", (O_WRONLY|O_CREAT|O_EXCL), 0644);
+
+        if (fd2 == -1) {
+            perror(argv[2]);
+            close(fd1);
+            return 2;
+        }
+        
+        dup2(fd2, STDOUT_FILENO);
+    }
+
+
+    while ((nr = read(fd1, buffer, BUFFER_SIZE)) > 0){
+        if ((nw = write(STDOUT_FILENO, buffer, nr)) <= 0 || nw != nr) {
+            perror("Error");
+            close(fd1);
+            return 4;
+        }
+    }
+    close(fd1);
+    return 0;
 }
 
