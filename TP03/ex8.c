bbb836e59d674d088c067b2aef1f83f02f59047a
Mariana Truta <marianatruta2000@gmail.com>
2020-03-11 09:59:04 +0000

    added ex8 and ex9

--- TP03/ex8.c
+++ TP03/ex8.c
@@ -12,12 +12,19 @@ int main(int argc, char *argv[], char *envp[]) {
     }
 
     pid=fork();
+    char *command[] = {"ls", "-laR", NULL};
 
     if (pid > 0)
         printf("My child is going to execute command \"ls -laR %s\"\n", argv[1]);
     
     else if (pid == 0){
-        execlp("ls","-laR", argv[1], NULL); //o processo‐filho faz o que o processo‐pai anuncia que ele vai fazer --> alinea a
+        //o processo‐filho faz o que o processo‐pai anuncia que ele vai fazer
+        execlp("ls", "ls, ""-laR", argv[1], NULL); // alinea a
+        //execl("/bin/ls","ls","-laR",argv[1],NULL); //alinea b
+        //execvp("ls", command); //alinea c
+        //execv("/bin/ls", command); //alinea d
+        //execve("/bin/ls", command, envp); //alinea e
+
         printf("Command not executed !\n");
         exit(1);
     }
---
+++ TP03/ex9.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
+
+
+int main(int argc, char *argv[]) {
+
+    pid_t pid, childpid;
+    int status;
+
+    if (argc != 2) {
+        printf("usage: %s dirname\n",argv[0]);
+        exit(1);
+    }
+
+    pid=fork();
+    
+
+    if (pid > 0){
+        printf("My child is going to execute command \"ls -laR %s\"\n", argv[1]);
+        childpid = wait(&status);
+
+        if(WIFEXITED(status)){
+            printf("A child with PID %d terminated normally with EXIT CODE %d\n", childpid, WEXITSTATUS(status)); // WEXITSTATUS(status) => permite obter o exit status do filho
+        }
+        else if(WIFSIGNALED(status)){
+            printf("A child with PID %d terminated abnormally with SIGNAL NUMBER %d\n", childpid, WTERMSIG(status));
+        }
+        else if(WIFSTOPPED(status)){
+            printf("A child with PID %d stopped because of the SIGNAL NUMBER %d\n", childpid, WSTOPSIG(status));
+        }
+
+
+        
+    }
+    else if (pid == 0){
+        execlp("ls", "ls", "-laR", argv[1], NULL);
+        printf("Command not executed !\n");
+        exit(1);
+    }
+
+    exit(0);
+}
+
+
+//nota alinea a: para matar o filho, é necessario fazer ps -u no terminal, encontrar o pid do filho e executar o comando "kill pidfilho"
